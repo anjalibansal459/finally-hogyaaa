330eb9e3b3563a108206288f5108ec775a0991c1
anjalibansal459 <76689035+anjalibansal459@users.noreply.github.com>
2021-07-12 14:42:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,81 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint=Matter.Constraint;
+var treeObj, stoneObj,groundObject, launcherObject;
+var mango1,mango2,mango3,mango4,mango5,mango6
+var world,boy;
+var stone ,slingShot
+function preload(){
+	boy=loadImage("boy.png");
+  }
+
+function setup() {
+	createCanvas(1300, 600);
+	engine = Engine.create();
+	world = engine.world;
+
+	mango1=new mango(1100,100,30);
+	mango2=new mango(1000,100,30);
+	mango3=new mango(1000,200,30);
+	mango4=new mango(1100,200,30);
+	mango5=new mango(1200,200,30);
+	mango6=new mango(900,200,30);
+	treeObj=new tree(1050,580);
+	groundObject=new ground(width/2,600,width,20);
+	stone=new Stone(250,200,20,20)
+	slingShot=new Shot(stone.body,{x:250,y:400})
+	Engine.run(engine);
+
+}
+
+function draw() {
+
+  background(230);
+  //Add code for displaying text here!
+  image(boy ,200,340,200,300);
+  
+
+  treeObj.display();
+  mango1.display();
+  mango2.display();
+  mango3.display();
+  mango4.display();
+  mango5.display();
+  mango6.display();
+stone.display()
+slingShot.display()
+  groundObject.display();
+  detectCollision(stone,mango1);
+  detectCollision(stone,mango2);
+  detectCollision(stone,mango3);
+  detectCollision(stone,mango4);
+  detectCollision(stone,mango5);
+  detectCollision(stone,mango6);
+}
+function mouseDragged(){
+    Matter.Body.setPosition(stone.body,{x:mouseX,y:mouseY});
+}
+
+function mouseReleased(){
+    slingShot.fly();
+}
+function detectCollision(lstone,lmango){
+	mangoBodyPosition=lmango.body.position
+	stoneBodyPosition=lstone.body.position
+	var distance=dist(stoneBodyPosition.x,stoneBodyPosition.y,mangoBodyPosition.x,mangoBodyPosition.y)
+	if(distance<=lmango.r+lstone.r){
+		Matter.Body.setStatic(lmango.body,false);
+	}
+}
+
+function keyPressed(){
+
+	if(keyCode === 32){
+		Matter.Body.setPosition(stone.body,{x:150,y:550})
+		slingShot.attach(stone.body);
+	}
+}
